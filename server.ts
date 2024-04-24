4717d8978caba7391111b284302ff0d06da2ae9d
Akshat Mittal <akshatmittal2506@gmail.com>
2024-04-24 06:38:36 +0530

    feat: created config service

--- server.ts
+++ server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { PORT } from "./config";
 
 const app = express();
 
@@ -6,6 +7,6 @@ app.get("/api/health", (req, res) => {
 	return res.status(200).json({ message: "API is healthy" });
 });
 
-app.listen(4000, () =>
-	console.info(`Server is listening at http://localhost:4000`)
+app.listen(PORT, () =>
+	console.info(`Server is listening at http://localhost:${PORT}`)
 );
