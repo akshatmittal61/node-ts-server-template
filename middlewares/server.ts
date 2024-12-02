b3be770eebdd7250a2a3b023fd7e07dea294d09f
Akshat Mittal <akshatmittal2506@gmail.com>
2024-12-02 16:02:23 +0530

    fix: server and db connections

--- middlewares/server.ts
+++ middlewares/server.ts
@@ -1,7 +1,7 @@
 import { default as corsMiddleware } from "cors";
 import { NextFunction } from "express";
+import { DatabaseManager } from "../connections";
 import { HTTP } from "../constants";
-import { DatabaseManager } from "../db";
 import { Logger } from "../log";
 import { ApiRequest, ApiResponse } from "../types";
 
@@ -27,14 +27,23 @@ export const cors = corsMiddleware({
 	origin: "*",
 });
 
-export const useDb = (_: ApiRequest, res: ApiResponse, next: NextFunction) => {
-	if (DatabaseManager.isReady() === false) {
-		return res
-			.status(HTTP.status.SERVICE_UNAVAILABLE)
-			.json({ message: HTTP.message.DB_CONNECTION_ERROR });
-	}
-	return next();
-};
+export const useDb =
+	(db: DatabaseManager) =>
+	(_: ApiRequest, res: ApiResponse, next: NextFunction) => {
+		if (db.isConnected() === false) {
+			Logger.error("Unable to connect to database");
+			return db.connect().then((isConnected) => {
+				if (isConnected) {
+					return next();
+				} else {
+					return res
+						.status(HTTP.status.SERVICE_UNAVAILABLE)
+						.json({ message: HTTP.message.DB_CONNECTION_ERROR });
+				}
+			});
+		}
+		return next();
+	};
 
 export const tracer = (req: ApiRequest, _: ApiResponse, next: NextFunction) => {
 	Logger.debug(req.method, req.path, req.params, req.body, req.headers);
--- server.ts
+++ server.ts
@@ -1,44 +1,5 @@
-import express from "express";
-import { PORT } from "./config";
-import { ServerController } from "./controllers";
-import { DatabaseManager } from "./db";
-import { Logger } from "./log";
-import {
-	cors,
-	errorHandler,
-	parseCookies,
-	profiler,
-	tracer,
-	useDb,
-} from "./middlewares";
-import { apiRouter } from "./routes";
+import { dbUri, PORT } from "./config";
+import { Server } from "./connections";
 
-const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(tracer);
-app.use(cors);
-app.use(parseCookies);
-app.use(profiler);
-
-app.get("/api/health", ServerController.health);
-app.use(useDb);
-app.get("/api/heartbeat", ServerController.heartbeat);
-app.use("/api/v1", apiRouter);
-app.use(errorHandler);
-
-const init = async () => {
-	Logger.info(`Server listening on port ${PORT}`);
-	const connectionStatus = await DatabaseManager.connect();
-	if (connectionStatus) {
-		Logger.info("MongoDB connected");
-	} else {
-		Logger.error("Database connection failed");
-		Logger.info("Server is running without database");
-	}
-};
-
-app.listen(PORT, () => {
-	init();
-});
+const server = new Server(PORT, dbUri);
+server.start();
